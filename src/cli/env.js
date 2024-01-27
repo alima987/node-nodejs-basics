596719e76290c52961a1c2d9352f3d6af85b7488
alima987 <alima1996b@gmail.com>
2024-01-27 13:15:41 +0500

    feat: complete env task

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-
+    const prefix = 'RSS_';
+    const envVars = Object.entries(process.env)
+      .filter(([key]) => key.startsWith(prefix))
+      .map(([key, value]) => `${key}=${value}`)
+      .join('; ');
+    
+    console.log(envVars);
 };
 
 parseEnv();
