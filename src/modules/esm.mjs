ebcc6bbc5a71cfbefe5dbe3dd574f1a4ff359181
alima987 <alima1996b@gmail.com>
2024-01-30 01:04:50 +0500

    fix: fix esm.mjs file

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,37 +1,44 @@
-import { createRequire } from 'node:module'
-import path from 'path';
-import { release, version } from 'os';
-import { createServer as createServerHttp } from 'http';
-import './files/c.js';
-
-const random = Math.random();
-
-let unknownObject;
-const require = createRequire(import.meta.url)
-if (random > 0.5) {
-    unknownObject = require('./files/a.json');
-} else {
-    unknownObject = require('./files/b.json');
-}
-
-console.log(`Release ${release()}`);
-console.log(`Version ${version()}`);
-console.log(`Path segment separator is "${path.sep}"`);
-
-console.log(`Path to current file is ${import.meta.url}`);
-console.log(`Path to current directory is ${path.dirname(import.meta.url)}`);
-
-const myServer = createServerHttp((_, res) => {
-    res.end('Request accepted');
-});
-
-const PORT = 3000;
-
-console.log(unknownObject);
-
-myServer.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-    console.log('To terminate it, use Ctrl+C combination');
-});
-
+import path from 'path'; 
+import { release, version } from 'os'; 
+import { createServer as createServerHttp } from 'http'; 
+import './files/c.js'; 
+import { fileURLToPath } from 'url'; 
+ 
+const __filename = fileURLToPath(import.meta.url); 
+const __dirname = path.dirname(__filename); 
+ 
+const random = Math.random(); 
+ 
+let unknownObject; 
+ 
+if (random > 0.5) { 
+    unknownObject = await import('./files/a.json', { 
+     assert: { type: 'json' }, 
+    }); 
+} else { 
+    unknownObject = await import('./files/b.json', { 
+     assert: { type: 'json' }, 
+    }); 
+} 
+ 
+console.log(`Release ${release()}`); 
+console.log(`Version ${version()}`); 
+console.log(`Path segment separator is "${path.sep}"`); 
+ 
+console.log(`Path to current file is ${__filename}`); 
+console.log(`Path to current directory is ${__dirname}`); 
+ 
+const myServer = createServerHttp((_, res) => { 
+    res.end('Request accepted'); 
+}); 
+ 
+const PORT = 3000; 
+ 
+console.log(unknownObject); 
+ 
+myServer.listen(PORT, () => { 
+    console.log(`Server is listening on port ${PORT}`); 
+    console.log('To terminate it, use Ctrl+C combination'); 
+}); 
+ 
 export { unknownObject, myServer };
